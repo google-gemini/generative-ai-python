70cc6d5b0174dc2c28591989ed74d2cb8d87a873
Mark Daoust <markdaoust@google.com>
2023-06-02 13:47:20 -0700

    Add future annotations for py3.9

--- google/generativeai/types/discuss_types.py
+++ google/generativeai/types/discuss_types.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Type definitions for the discuss service."""
+from __future__ import annotations
 
 import abc
 import dataclasses
