2e9f23ef5f246263a528c481b9657d669af8be75
Mark Daoust <markdaoust@google.com>
2023-10-17 20:39:25 -0700

    Restore ModelNameOptions for backwards compatibility. (#87)

--- google/generativeai/types/model_types.py
+++ google/generativeai/types/model_types.py
@@ -30,6 +30,7 @@ from google.generativeai import string_utils
 
 __all__ = [
     "Model",
+    "ModelNameOptions",
     "AnyModelNameOptions",
     "BaseModelNameOptions",
     "TunedModelNameOptions",
@@ -310,6 +311,7 @@ class Hyperparameters:
 BaseModelNameOptions = Union[str, Model, glm.Model]
 TunedModelNameOptions = Union[str, TunedModel, glm.TunedModel]
 AnyModelNameOptions = Union[str, Model, glm.Model, TunedModel, glm.TunedModel]
+ModelNameOptions = AnyModelNameOptions
 
 
 def make_model_name(name: AnyModelNameOptions):
