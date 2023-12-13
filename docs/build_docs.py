07fb91cc6e4aee5d656f5c5aa5c1df851ffa494b
Mark Daoust <markdaoust@google.com>
2023-12-13 03:52:54 -0800

    Docstring (#110)
    * Update docstring
    
    * update docstrings
    
    * 0.3.1

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -26,7 +26,7 @@ import re
 import textwrap
 import typing
 
-# For showing the conditional imports and types in `content_types.py` 
+# For showing the conditional imports and types in `content_types.py`
 typing.TYPE_CHECKING = True
 
 from absl import app
--- google/generativeai/types/generation_types.py
+++ google/generativeai/types/generation_types.py
@@ -54,6 +54,64 @@ class GenerationConfigDict(TypedDict):
 
 @dataclasses.dataclass
 class GenerationConfig:
+    """A simple dataclass used to configure the generation parameters of `GenerativeModel.generate_content`.
+
+    Attributes:
+        candidate_count:
+            Number of generated responses to return.
+        stop_sequences:
+            The set of character sequences (up
+            to 5) that will stop output generation. If
+            specified, the API will stop at the first
+            appearance of a stop sequence. The stop sequence
+            will not be included as part of the response.
+        max_output_tokens:
+            The maximum number of tokens to include in a
+            candidate.
+
+            If unset, this will default to output_token_limit specified
+            in the model's specification.
+        temperature:
+            Controls the randomness of the output. Note: The
+
+            default value varies by model, see the `Model.temperature`
+            attribute of the `Model` returned the `genai.get_model`
+            function.
+
+            Values can range from [0.0,1.0], inclusive. A value closer
+            to 1.0 will produce responses that are more varied and
+            creative, while a value closer to 0.0 will typically result
+            in more straightforward responses from the model.
+        top_p:
+            Optional. The maximum cumulative probability of tokens to
+            consider when sampling.
+
+            The model uses combined Top-k and nucleus sampling.
+
+            Tokens are sorted based on their assigned probabilities so
+            that only the most likely tokens are considered. Top-k
+            sampling directly limits the maximum number of tokens to
+            consider, while Nucleus sampling limits number of tokens
+            based on the cumulative probability.
+
+            Note: The default value varies by model, see the
+            `Model.top_p` attribute of the `Model` returned the
+            `genai.get_model` function.
+
+        top_k (int):
+            Optional. The maximum number of tokens to consider when
+            sampling.
+
+            The model uses combined Top-k and nucleus sampling.
+
+            Top-k sampling considers the set of `top_k` most probable
+            tokens. Defaults to 40.
+
+            Note: The default value varies by model, see the
+            `Model.top_k` attribute of the `Model` returned the
+            `genai.get_model` function.
+    """
+
     candidate_count: int | None = None
     stop_sequences: Iterable[str] | None = None
     max_output_tokens: int | None = None
