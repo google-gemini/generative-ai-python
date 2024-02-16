804fd2a1ad7de1dbd6093ec685f5f5521245708d
Mayuresh Agashe <magashe@google.com>
2024-02-17 01:44:57 +0530

    Move create_permission checks
    moved create_permission checks to _make_create_permission_request

--- google/generativeai/types/retriever_types.py
+++ google/generativeai/types/retriever_types.py
@@ -498,6 +498,24 @@ class Corpus:
 
         if grantee_type:
             grantee_type = permission_types.to_grantee_type(grantee_type)
+        
+        if (
+            email_address
+            and grantee_type
+            == permission_types.GranteeType.EVERYONE
+        ):
+            raise ValueError(
+                f"Cannot limit access for: `{email_address}` when `grantee_type` is set to `EVERYONE`."
+            )
+
+        if (
+            not email_address
+            and grantee_type
+            != permission_types.GranteeType.EVERYONE
+        ):
+            raise ValueError(
+                f"`email_address` must be specified unless `grantee_type` is set to `EVERYONE`."
+            )
 
         permission = glm.Permission(
             role=role,
@@ -535,24 +553,6 @@ class Corpus:
         if client is None:
             client = get_dafault_permission_client()
 
-        if (
-            email_address
-            and permission_types.to_grantee_type(grantee_type)
-            == permission_types.GranteeType.EVERYONE
-        ):
-            raise ValueError(
-                f"Cannot limit access for: `{email_address}` when `grantee_type` is set to `EVERYONE`."
-            )
-
-        if (
-            not email_address
-            and permission_types.to_grantee_type(grantee_type)
-            != permission_types.GranteeType.EVERYONE
-        ):
-            raise ValueError(
-                f"`email_address` must be specified unless `grantee_type` is set to `EVERYONE`."
-            )
-
         request = self._make_create_permission_request(
             role=role, grantee_type=grantee_type, email_address=email_address
         )
@@ -572,24 +572,6 @@ class Corpus:
         """
         if client is None:
             client = get_dafault_permission_async_client()
-
-        if (
-            email_address
-            and permission_types.to_grantee_type(grantee_type)
-            == permission_types.GranteeType.EVERYONE
-        ):
-            raise ValueError(
-                f"Cannot limit access for: `{email_address}` when `grantee_type` is set to `EVERYONE`."
-            )
-
-        if (
-            not email_address
-            and permission_types.to_grantee_type(grantee_type)
-            != permission_types.GranteeType.EVERYONE
-        ):
-            raise ValueError(
-                f"`email_address` must be specified unless `grantee_type` is set to `EVERYONE`."
-            )
 
         request = self._make_create_permission_request(
             role=role, grantee_type=grantee_type, email_address=email_address
