b28f2d1395d888fe149a031afbec359e1b523f8e
Mark Daoust <markdaoust@google.com>
2024-02-21 16:59:20 -0800

    Remove init, PEP420PackageFinder handles this + it's breaking local dev installs (pip install -e .) (#200)
    * Remove init, PEP420PackageFinder handles this
    
    * fix pytype
    
    * pytype needs an __init__.py

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# pytype: skip-file
 r"""Api reference docs generation script, using tensorflow_docs
 
 This script generates API reference docs for the reference doc generator.
