c854806ba61a67afd0a7ade2395bb24ebd5a2e35
Shilpa Kancharla <snkancharla@google.com>
2024-03-08 14:13:22 -0800

    remove print statements

--- google/generativeai/types/retriever_types.py
+++ google/generativeai/types/retriever_types.py
@@ -251,9 +251,7 @@ class Corpus:
             raise ValueError(NAME_ERROR_MSG.format(length=len(name), name=name))
 
         request = glm.CreateDocumentRequest(parent=self.name, document=document)
-        print(request)
         response = client.create_document(request)
-        print(response)
         return decode_document(response)
 
     async def create_document_async(
@@ -565,10 +563,8 @@ class Corpus:
 
 def decode_document(document):
     document = type(document).to_dict(document)
-    print(document)
     idecode_time(document, "create_time")
     idecode_time(document, "update_time")
-    print(Document(**document))
     return Document(**document)
 
 
