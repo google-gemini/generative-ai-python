41d93a76209ede61b785e54d72e7992b67ab417f
Mark Daoust <markdaoust@google.com>
2024-03-28 10:25:01 -0700

    Add lifetime instructions

--- tests/test_typing_extensions.py
+++ tests/test_typing_extensions.py
@@ -18,17 +18,27 @@ import re
 
 from absl.testing import absltest
 
-TYPING_RE = re.compile(r'\btyping\b.*TypedDict')
+TYPING_RE = re.compile(r"\btyping\b.*TypedDict")
+
 
 class TypingExtensionsTests(absltest.TestCase):
+    """Pydantic users need the improved version of TypedDict, from typing_extensions.
+
+    This is only required for python versions <3.12. Once 3.12 is the lowest supported version we can drop this.
+
+    ref: https://docs.pydantic.dev/2.3/usage/types/dicts_mapping/
+
+    > the typing-extensions package is required for Python <3.12
+    """
+
     def test_no_typing_typed_dict(self):
         root = pathlib.Path(__file__).parent.parent
         for fpath in root.rglob("*.py"):
-            print(fpath)
             source = fpath.read_text()
             if match := TYPING_RE.search(source):
-                raise ValueError(f"Please import `TypedDict` from `typing_extensions` not `typing`, in:", fpath)
-
+                raise ValueError(
+                    f"Please import `TypedDict` from `typing_extensions` not `typing`, in:", fpath
+                )
 
 
 if __name__ == "__main__":
