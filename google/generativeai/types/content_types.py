097d15a54d1857403e5066fcdc970b788be6334b
Mayuresh Agashe <magashe@google.com>
2024-04-04 12:47:32 +0530

    Replace alias with concrete data types for instance checks

--- google/generativeai/types/content_types.py
+++ google/generativeai/types/content_types.py
@@ -706,7 +706,7 @@ FunctionCallingConfigType = Union[
 def to_function_calling_config(obj: FunctionCallingConfigType) -> glm.FunctionCallingConfig:
     if isinstance(obj, glm.FunctionCallingConfig):
         return obj
-    elif isinstance(obj, FunctionCallingModeType):
+    elif isinstance(obj, (FunctionCallingMode, str, int)):
         obj = {"mode": to_function_calling_mode(obj)}
     elif isinstance(obj, dict):
         obj = obj.copy()
