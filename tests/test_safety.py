d3616d039ea5d8a68338251348f10143ea93d6d1
Mark Daoust <markdaoust@google.com>
2024-05-17 13:20:06 -0700

    license
    Change-Id: Ifa4843831b9c1479198c2b45c5b5abad8410f448

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -1,19 +1,22 @@
-import collections
-from collections.abc import Iterable
-import copy
-import pathlib
-from typing import Any
-import textwrap
-import unittest.mock
+# -*- coding: utf-8 -*-
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from absl.testing import absltest
 from absl.testing import parameterized
 import google.ai.generativelanguage as glm
-from google.generativeai import client as client_lib
-from google.generativeai import generative_models
 from google.generativeai.types import safety_types
-from google.generativeai.types import generation_types
-
-import PIL.Image
 
 
 class SafetyTests(parameterized.TestCase):
