c10db9292977462b276be017841733939e51bb9d
Mark McDonald <macd@google.com>
2024-05-27 10:18:44 +0800

    Grammar

--- google/generativeai/__init__.py
+++ google/generativeai/__init__.py
@@ -31,7 +31,7 @@ import os
 genai.configure(api_key=os.environ['API_KEY'])
 
 model = genai.GenerativeModel(name='gemini-1.5-flash')
-response = model.generate_content('Teach me about how a LLM works')
+response = model.generate_content('Teach me about how an LLM works')
 
 print(response.text)
 ```
