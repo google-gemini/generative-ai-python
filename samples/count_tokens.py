9070fd5353bb4e753f688c0b6bd3b2a0adcd598a
Shilpa Kancharla <snkancharla@google.com>
2024-06-27 13:36:18 -0700

    Add media samples and update count_tokens samples. Still need to work on caching

--- samples/count_tokens.py
+++ samples/count_tokens.py
@@ -15,35 +15,69 @@
 from absl.testing import absltest
 
 import google.generativeai as genai
+import pathlib
+
+media = pathlib.Path(__file__).parents[1] / "third_party"
 
 class UnitTests(absltest.TestCase):
     def test_tokens_text_only(self):
         # [START tokens_text_only]
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
+        print(model.count_tokens("The quick brown fox jumps over the lazy dog."))
         # [END tokens_text_only]
 
     def test_tokens_chat(self):
         # [START tokens_chat]
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
+        chat = model.start_chat(history=[{'role':'user', 
+                                          'parts':'Hi, my name is Bob.'},  
+                                        {'role':'model', 
+                                         'parts':'Hi Bob!'}])
+        model.count_tokens(chat.history)
+
+        from google.generativeai.types.content_types import to_contents
+        model.count_tokens(chat.history + to_contents('What is the meaning of life?'))
         # [END tokens_chat]
 
     def test_tokens_multimodal_image_inline(self):
         # [START tokens_multimodal_image_inline]
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
+        import PIL
+        organ = PIL.Image.open(media / 'organ.jpg')
+        print(model.count_tokens(['Tell me about this instrument', organ]))
         # [END tokens_multimodal_image_inline]
 
     def test_tokens_multimodal_image_file_api(self):
         # [START tokens_multimodal_image_file_api]
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
+        organ_upload = genai.upload_file(media / 'organ.jpg')
+        print(model.count_tokens(['Tell me about this instrument', organ_upload]))
         # [END tokens_multimodal_image_file_api]
 
     def test_tokens_video_audio_file_api(self):
         # [START tokens_video_audio_file_api]
+        model = genai.GenerativeModel('models/gemini-1.5-flash')
+        audio_upload = genai.upload_file(media / 'sample.mp3')
+        print(model.count_tokens(audio_upload))
         # [END tokens_video_audio_file_api]
 
-    def test_tokens_multimodal_image_inline(self):
-        # [START tokens_multimodal_image_inline]
-        # [END tokens_multimodal_image_inline]
+    def test_tokens_cached_content(self):
+        # [START tokens_cached_content]
+        # [END tokens_cached_content]
 
-    def test_count_tokens_and_usage_metadata(self):
-        # [START count_tokens_and_usage_metadata]
-        # [END count_tokens_and_usage_metadata]
+    def test_tokens_cached_system_instruction(self):
+        # [START tokens_cached_system_instruction]
+        print(genai.GenerativeModel().count_tokens("The quick brown fox jumps over the lazy dog."))
+        print(genai.GenerativeModel(
+            system_instruction='Talk like a pirate!'
+            ).count_tokens(
+                "The quick brown fox jumps over the lazy dog."
+                ))
+        # [END tokens_cached_system_instruction]
+
+    def test_tokens_cached_tools(self):
+        # [START tokens_cached_tools]
+        # [END tokens_cached_tools]
 
 if __name__ == "__main__":
     absltest.main()
