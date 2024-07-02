669a4af6d16d79caf14c2c36f0314cd5b4b353e3
Shilpa Kancharla <snkancharla@google.com>
2024-07-02 14:53:30 -0400

    Add REST sample for listing and getting models - skeleton

---
+++ rest_samples/models.sh
@@ -0,0 +1,5 @@
+# List models
+curl https://generativelanguage.googleapis.com/v1beta/models?key=$GOOGLE_API_KEY
+
+# Get models
+curl https://generativelanguage.googleapis.com/v1beta/models/gemini-pro?key=$GOOGLE_API_KEY
