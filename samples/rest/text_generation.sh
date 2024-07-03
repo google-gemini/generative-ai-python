4c222634c3f8b881bd4d52aec748e5b45ed82e6e
Shilpa Kancharla <snkancharla@google.com>
2024-07-03 16:41:27 -0400

    Add example for one image in text gen

--- samples/rest/text_generation.sh
+++ samples/rest/text_generation.sh
@@ -15,8 +15,23 @@ curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generat
 echo "[START text_gen_text_only_prompt_streaming]"
 # [START text_gen_text_only_prompt_streaming]
 
-!curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?alt=sse&key=${GOOGLE_API_KEY}" \
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?alt=sse&key=${GOOGLE_API_KEY}" \
         -H 'Content-Type: application/json' \
         --no-buffer \
         -d '{ "contents":[{"parts":[{"text": "Write a story about a magic backpack."}]}]}'
 # [END text_gen_text_only_prompt_streaming]
+
+echo "[START text_gen_multimodal_one_image_prompt]"
+# [START text_gen_multimodal_one_image_prompt]
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$GOOGLE_API_KEY" \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d '{
+      "contents": [{
+        "parts":[
+            {"text": "Tell me about this instrument"},
+            {"inline_data": "../third_party/organ.jpg}
+        ]
+        }]
+       }' 2> /dev/null
+# [END text_gen_multimodal_one_image_prompt]
