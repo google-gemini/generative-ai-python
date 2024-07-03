41952d9b96717d743966f8fb00b4ce8be2ca8ce1
Shilpa Kancharla <snkancharla@google.com>
2024-07-03 16:34:33 -0400

    Start on text_generation samples for shell

---
+++ samples/rest/text_generation.sh
@@ -0,0 +1,22 @@
+set -eu
+
+echo "[START text_gen_text_only_prompt]"
+# [START text_gen_text_only_prompt]
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$GOOGLE_API_KEY" \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d '{
+      "contents": [{
+        "parts":[{"text": "Write a story about a magic backpack."}]
+        }]
+       }' 2> /dev/null
+# [END text_gen_text_only_prompt]
+
+echo "[START text_gen_text_only_prompt_streaming]"
+# [START text_gen_text_only_prompt_streaming]
+
+!curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?alt=sse&key=${GOOGLE_API_KEY}" \
+        -H 'Content-Type: application/json' \
+        --no-buffer \
+        -d '{ "contents":[{"parts":[{"text": "Write a story about a magic backpack."}]}]}'
+# [END text_gen_text_only_prompt_streaming]
