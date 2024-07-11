dba9a860938a5996fa95ff5cded67183e44e4c5f
Mark Daoust <markdaoust@google.com>
2024-07-11 11:18:59 -0700

    last message should be 'role:user'
    Change-Id: I3e06e9e0ffb553cfc70add5ed0365cb56e9fddff

--- samples/rest/chat.sh
+++ samples/rest/chat.sh
@@ -1,6 +1,7 @@
 set -eu
 
-IMG_PATH=$(realpath ~/generative-ai-python/third_party/organ.jpg)
+SCRIPT_DIR=$(dirname "$0")
+MEDIA_DIR=$(realpath ${SCRIPT_DIR}/../../third_party)
 
 echo "[START chat]"
 # [START chat]
@@ -15,6 +16,9 @@ curl https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:ge
         {"role": "model",
          "parts":[{
            "text": "Great to meet you. What would you like to know?"}]},
+        {"role":"user",
+         "parts":[{
+           "text": "I have two dogs in my house. How many paws are in my house?"}]},
       ]
     }' 2> /dev/null | grep "text"
 # [END chat]
@@ -32,31 +36,58 @@ curl https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:st
         {"role": "model",
          "parts":[{
            "text": "Great to meet you. What would you like to know?"}]},
+        {"role":"user",
+         "parts":[{
+           "text": "I have two dogs in my house. How many paws are in my house?"}]},
       ]
     }' 2> /dev/null | grep "text"
 # [END chat_streaming]
 
 echo "[START chat_streaming_with_images]"
 # [START chat_streaming_with_images]
+IMG_PATH=${MEDIA_DIR}/organ.jpg
+
+if [[ "$(base64 --version 2>&1)" = *"FreeBSD"* ]]; then
+  B64FLAGS="--input"
+else
+  B64FLAGS="-w0"
+fi
+
 curl https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:streamGenerateContent?key=$GOOGLE_API_KEY \
     -H 'Content-Type: application/json' \
     -X POST \
     -d '{
-      "contents": [
-        {"role":"user",
-         "parts":[{
-           "text": "Hello"}]},
-        {"role": "model",
-         "parts":[
-            {"text": "Tell me about this instrument"},
+        "contents": [
             {
-              "inline_data": {
-                "mime_type":"image/jpeg",
-                "data": "'$(base64 -w0 $IMG_PATH)'"
-              }
+                "role": "user",
+                "parts": [
+                    {
+                        "text": "Hello, I am interested in learning about musical instruments. Can I show you one?"
+                    }
+                ]
+            },
+            {
+                "role": "model",
+                "parts": [
+                    {
+                        "text": "Certainly."
+                    },
+                ]
+            },
+            {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": "Tell me about this instrument"
+                    },
+                    {
+                        "inline_data": {
+                            "mime_type": "image/jpeg",
+                            "data": "'$(base64 $B64FLAGS $IMG_PATH)'"
+                        }
+                    }
+                ]
             }
-          }
         ]
-      ]
     }' 2> /dev/null | grep "text"
 # [END chat_streaming_with_images]
