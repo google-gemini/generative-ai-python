2617ca9b7a3563ccc0dd2860e133b565afaa2f4a
Shilpa Kancharla <snkancharla@google.com>
2024-07-25 11:43:48 +0100

    Code execution for shell

---
+++ samples/rest/code_execution.sh
@@ -0,0 +1,20 @@
+set -eu 
+
+echo "[START code_execution_basic]"
+# [START code_execution_basic]
+
+# [END code_execution_basic]
+
+echo "[START code_execution_request_override]"
+# [START code_execution_request_override]
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=$GOOGLE_API_KEY" \
+-H 'Content-Type: application/json' \
+-d ' {"tools": ["code_execution"],
+    "contents": {
+      "parts": 
+        {"text": "What is the sum of the first 50 prime numbers? "
+                "Generate and run code for the calculation, and make sure you get all 50."}
+                }
+        },
+    }'
+# [END code_execution_request_override]
