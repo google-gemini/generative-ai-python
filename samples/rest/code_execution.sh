39add6ac31ef93b5691089b77080ce42ea94452e
Mark Daoust <markdaoust@google.com>
2024-07-31 15:53:16 -0700

    Fix example
    Change-Id: I89d38b2fb38be407583ad1522b8b00a19efed03d

--- samples/rest/code_execution.sh
+++ samples/rest/code_execution.sh
@@ -4,12 +4,13 @@ echo "[START code_execution_basic]"
 # [START code_execution_basic]
 curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key=$GOOGLE_API_KEY" \
 -H 'Content-Type: application/json' \
--d ' {"tools": ["code_execution"],
+-d ' {
+    "tools": [{"code_execution": {}}],
     "contents": {
-      "parts": 
-        {"text": "What is the sum of the first 50 prime numbers? "
-                "Generate and run code for the calculation, and make sure you get all 50."}
-                }
-        },
-    }'
+        "parts": {
+            "text": "What is the sum of the first 50 prime numbers? Generate
+                     and run code for the calculation, and make sure you get all 50."
+        }
+    }
+}'
 # [END code_execution_basic]
