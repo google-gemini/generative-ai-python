f1f4124a60b676ed7a8ed65ab20bd0961fc6db7e
Mark Daoust <markdaoust@google.com>
2024-09-19 17:08:57 -0700

    Fix tests
    Change-Id: If8fbbba1966aa42601adec877e60d851d4f03b72

--- google/generativeai/types/model_types.py
+++ google/generativeai/types/model_types.py
@@ -195,6 +195,7 @@ class TunedModel:
     create_time: datetime.datetime | None = None
     update_time: datetime.datetime | None = None
     tuning_task: TuningTask | None = None
+    reader_project_numbers: List[int] | None = None
 
     @property
     def permissions(self) -> permission_types.Permissions:
