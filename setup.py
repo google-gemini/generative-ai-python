c8eadc4ab76eb6140b2702614bf081fd65804280
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-10-09 01:23:33 +0200

    feat: add py.typed (#509)
    * Update setup.py
    
    * Create py.typed
    
    * Update setup.py to trigger CI.
    
    ---------
    
    Co-authored-by: Mark Daoust <markdaoust@google.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License at:
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -88,6 +88,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Typing :: Typed",
     ],
     platforms="Posix; MacOS X; Windows",
     packages=packages,
@@ -97,4 +98,5 @@ setuptools.setup(
     extras_require=extras_require,
     include_package_data=True,
     zip_safe=False,
+    package_data={"generativeai": ["py.typed"]},
 )
