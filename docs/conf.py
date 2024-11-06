af9ba4d9bd79012564202e684700313ed649134c
Shruti Vishwakarma <shwvkrm@gmail.com>
2024-11-06 12:40:17 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,33 +31,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 # html_static_path = ['_static']
 
-
-import re
-
-# def set_relative_links(app, docname, source):
-#     # Pattern to match the GitHub links
-#     pattern = r'https://github\.com/vi-shruti/generative-ai-python/blob/main/docs/api/google/generativeai/(.+?)\.md(#.+)?'
-    
-#     def replace_link(match):
-#         file_name = match.group(1)
-#         anchor = match.group(2) or ''
-        
-#         # If it's the GenerativeModel file, we don't need the file name in the link
-#         if file_name == 'GenerativeModel':
-#             return f'{anchor}'
-#         else:
-#             return f'/api/google/generativeai/{file_name}{anchor}'
-
-#     # Replace the links
-#     source[0] = re.sub(pattern, replace_link, source[0])
-
-
 def set_relative_links(app, docname, source):
-    source[0] = source[0].replace(
-        '../google/generativeai/GenerativeModel.md',
-        ''
-    )
-
     source[0] = source[0].replace(
         '../google/generativeai/',
         '/api/google/generativeai/'
