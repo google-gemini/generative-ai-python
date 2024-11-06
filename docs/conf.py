b27e084ec3b48cc0dce4bee1a652eb2e69b40060
Shruti Vishwakarma <shwvkrm@gmail.com>
2024-11-06 13:48:12 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-import re
-from sphinx.util import logging
+# import re
+# from sphinx.util import logging
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 project = 'Google Generative AI - Python'
 copyright = '2024, Google LLC'
@@ -14,6 +14,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     ]
+
+autosectionlabel_prefix_document = True
 
 myst_url_schemes = ["http", "https", "mailto"]
 myst_enable_extensions = [
@@ -37,39 +39,39 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 # html_static_path = ['_static']
 
-# def set_relative_links(app, docname, source):
-#     source[0] = source[0].replace(
-#         '../google/generativeai/',
-#         '/api/google/generativeai/'
-#     )
+def set_relative_links(app, docname, source):
+    source[0] = source[0].replace(
+        '../google/generativeai/',
+        '/api/google/generativeai/'
+    )
 
-
-# def setup(app):
-#     app.connect('source-read', set_relative_links)
-
-
-
-
-def update_internal_links(app, docname, source):
-    def replace_link(match):
-        path = match.group(1)
-        anchor = match.group(2) or ''
-        if path.endswith('.md'):
-            path = path[:-3] + '.html'
-        return f'/generative-ai-python{path}{anchor}'
-
-    def replace_relative_path(match):
-        return '/api/google/generativeai/'
-
-    # Pattern for links with potential anchors
-    link_pattern = r'(/api/google/generativeai/[\w-]+)(\.md)?(#[\w-]+)?'
-    # Pattern for relative paths
-    path_pattern = r'\.\.\/google\/generativeai\/'
-
-    # Replace links
-    source[0] = re.sub(link_pattern, replace_link, source[0])
-    # Replace relative paths
-    source[0] = re.sub(path_pattern, replace_relative_path, source[0])
 
 def setup(app):
-    app.connect('source-read', update_internal_links)
+    app.connect('source-read', set_relative_links)
+
+
+
+
+# def update_internal_links(app, docname, source):
+#     def replace_link(match):
+#         path = match.group(1)
+#         anchor = match.group(2) or ''
+#         if path.endswith('.md'):
+#             path = path[:-3] + '.html'
+#         return f'/generative-ai-python{path}{anchor}'
+
+#     def replace_relative_path(match):
+#         return '/api/google/generativeai/'
+
+#     # Pattern for links with potential anchors
+#     link_pattern = r'(/api/google/generativeai/[\w-]+)(\.md)?(#[\w-]+)?'
+#     # Pattern for relative paths
+#     path_pattern = r'\.\.\/google\/generativeai\/'
+
+#     # Replace links
+#     source[0] = re.sub(link_pattern, replace_link, source[0])
+#     # Replace relative paths
+#     source[0] = re.sub(path_pattern, replace_relative_path, source[0])
+
+# def setup(app):
+#     app.connect('source-read', update_internal_links)
