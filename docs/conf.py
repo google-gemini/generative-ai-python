22bd52ba160d2f61b58c217bc34a75c7450cec36
Shruti Vishwakarma <shwvkrm@gmail.com>
2024-11-06 16:22:28 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,30 +41,30 @@ html_theme = 'alabaster'
 # html_static_path = ['_static']
 
 
-# def set_relative_links(app, docname, source):
-#     source[0] = source[0].replace(
-#         '../google/generativeai/',
-#         '/api/google/generativeai/'
-#     )
+def set_relative_links(app, docname, source):
+    source[0] = source[0].replace(
+        '../google/generativeai/',
+        '/api/google/generativeai/'
+    )
 
-#     # if '#' in source[0]:
-#     #     print("\n\nComing from set_relative_links: ")
-#     #     print(source[0])
-#     #     print("==========================================\n\n")        
-
-# def setup(app):
-#     app.connect('source-read', set_relative_links)
-
-
-
-def set_relative_links(app, pagename, templatename, context, doctree):
-    if 'body' in context:
-        # Perform the substitution in the HTML body
-        context['body'] = re.sub(
-            r'(\.\./google/generativeai/[^"]+)\.md(#\w+)?',
-            r'/api/google/generativeai/\1.html\2',
-            context['body']
-        )
+    # if '#' in source[0]:
+    #     print("\n\nComing from set_relative_links: ")
+    #     print(source[0])
+    #     print("==========================================\n\n")        
 
 def setup(app):
-    app.connect('html-page-context', set_relative_links)
+    app.connect('source-read', set_relative_links)
+
+
+
+# def set_relative_links(app, pagename, templatename, context, doctree):
+#     if 'body' in context:
+#         # Perform the substitution in the HTML body
+#         context['body'] = re.sub(
+#             r'(\.\./google/generativeai/[^"]+)\.md(#\w+)?',
+#             r'/api/google/generativeai/\1.html\2',
+#             context['body']
+#         )
+
+# def setup(app):
+#     app.connect('html-page-context', set_relative_links)
