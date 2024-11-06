3fd0a25130e7d62f170c71b59d39f9eadb4720ed
Shruti Vishwakarma <shwvkrm@gmail.com>
2024-11-06 16:19:16 +0530

    Myst XRef
    Resolving

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import re
+
 project = 'Google Generative AI - Python'
 copyright = '2024, Google LLC'
 author = 'Google LLC'
@@ -6,14 +8,10 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.coverage',
     'myst_parser',
-    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     ]
 
-intersphinx_mapping = {
-    "generative-ai-python": ("https://vi-shruti.github.io/generative-ai-python/", None),
-}
 # autosectionlabel_prefix_document = True
 
 myst_url_schemes = ["http", "https", "mailto"]
@@ -26,7 +24,8 @@ myst_enable_extensions = [
     "smartquotes",
     "strikethrough",
     "tasklist",
-    "substitution"
+    "substitution",
+    "linkify",
 ]
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-header-anchors
 # Controls allowable header anchors in markdown files. Value allows header anchors for h1 - h6
@@ -42,16 +41,30 @@ html_theme = 'alabaster'
 # html_static_path = ['_static']
 
 
-def set_relative_links(app, docname, source):
-    source[0] = source[0].replace(
-        '../google/generativeai/',
-        '/api/google/generativeai/'
-    )
+# def set_relative_links(app, docname, source):
+#     source[0] = source[0].replace(
+#         '../google/generativeai/',
+#         '/api/google/generativeai/'
+#     )
 
-    if '#' in source[0]:
-        print("\n\nComing from set_relative_links: ")
-        print(source[0])
-        print("==========================================\n\n")        
+#     # if '#' in source[0]:
+#     #     print("\n\nComing from set_relative_links: ")
+#     #     print(source[0])
+#     #     print("==========================================\n\n")        
+
+# def setup(app):
+#     app.connect('source-read', set_relative_links)
+
+
+
+def set_relative_links(app, pagename, templatename, context, doctree):
+    if 'body' in context:
+        # Perform the substitution in the HTML body
+        context['body'] = re.sub(
+            r'(\.\./google/generativeai/[^"]+)\.md(#\w+)?',
+            r'/api/google/generativeai/\1.html\2',
+            context['body']
+        )
 
 def setup(app):
-    app.connect('source-read', set_relative_links)
+    app.connect('html-page-context', set_relative_links)
