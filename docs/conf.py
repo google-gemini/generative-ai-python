fea5e85c9d42b6117c089f518bf78208e12dc49c
Shruti Vishwakarma <shwvkrm@gmail.com>
2024-11-06 15:58:37 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,3 @@
-import re
-from sphinx.util import logging
-
-logger = logging.getLogger(__name__)
-
 project = 'Google Generative AI - Python'
 copyright = '2024, Google LLC'
 author = 'Google LLC'
@@ -11,10 +6,14 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.coverage',
     'myst_parser',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     ]
 
+intersphinx_mapping = {
+    "sphinx": ("https://vi-shruti.github.io/generative-ai-python/", None),
+}
 # autosectionlabel_prefix_document = True
 
 myst_url_schemes = ["http", "https", "mailto"]
@@ -43,43 +42,16 @@ html_theme = 'alabaster'
 # html_static_path = ['_static']
 
 
-# def set_relative_links(app, docname, source):
-#     source[0] = source[0].replace(
-#         '../google/generativeai/',
-#         '/api/google/generativeai/'
-#     )
+def set_relative_links(app, docname, source):
+    source[0] = source[0].replace(
+        '../google/generativeai/',
+        '/api/google/generativeai/'
+    )
 
-#     if '#' in source[0]:
-#         print("\n\nComing from set_relative_links: ")
-#         print(source[0])
-#         print("==========================================\n\n")        
-
-# def setup(app):
-#     app.connect('source-read', set_relative_links)
-
-
-def update_internal_links(app, docname, source):
-    def replace_link(match):
-        path = match.group(1)
-        anchor = match.group(2) or ''
-        if path.endswith('.md'):
-            path = path[:-3] + '.html'
-        return f'{path}{anchor}'
-        # return f'/generative-ai-python{path}{anchor}'
-
-    def replace_relative_path(match):
-        return '/api/google/generativeai/'
-
-    # Pattern for links with potential anchors
-    link_pattern = r'(\.\.\/google/generativeai/[\w-]+)(\.md)?(#[\w-]+)?'
-    # link_pattern = r'(/api/google/generativeai/[\w-]+)(\.md)?(#[\w-]+)?'
-    # Pattern for relative paths
-    path_pattern = r'\.\.\/google\/generativeai\/'
-
-    # Replace links
-    source[0] = re.sub(link_pattern, replace_link, source[0])
-    # Replace relative paths
-    source[0] = re.sub(path_pattern, replace_relative_path, source[0])
+    if '#' in source[0]:
+        print("\n\nComing from set_relative_links: ")
+        print(source[0])
+        print("==========================================\n\n")        
 
 def setup(app):
-    app.connect('source-read', update_internal_links)
+    app.connect('source-read', set_relative_links)
