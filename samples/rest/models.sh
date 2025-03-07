4ab3e54609b0420f8eae10534baa0568299b95e2
Logan Kilpatrick <lkilpatrick@google.com>
2025-03-07 15:06:15 -0600

    Update models.sh

--- samples/rest/models.sh
+++ samples/rest/models.sh
@@ -2,10 +2,10 @@ set -eu
 
 echo "[START models_list]"
 # [START models_list]
-curl https://generativelanguage.googleapis.com/v1beta/models?key=$GOOGLE_API_KEY
+curl https://generativelanguage.googleapis.com/v1beta/models?key=$GEMINI_API_KEY
 # [END models_list]
 
 echo "[START models_get]"
 # [START models_get]
-curl https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash?key=$GOOGLE_API_KEY
+curl https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash?key=$GEMINI_API_KEY
 # [END models_get]
